659752225078c012da52a98c1e605d499e8f6d27
heyanjing <1366162208@qq.com>
2017-04-24 10:38:35 +0800

    测试注解验证   usercontrller  reg

--- src/main/java/com/he/spring/Constants.java
+++ src/main/java/com/he/spring/Constants.java
@@ -11,7 +11,7 @@ import com.he.spring.util.Dates;
  */
 public interface Constants {
 	public static String	VERSION			= Dates.newDateString(DateInfo.NONE);
-	public static String	CONFIG_PROPS	= "config/config.properties";
+	public static String	CONFIG_PROPS	= "config/config-dev.properties";
 	public static boolean	APP_DEBUG		= Configs.getBoolean("app_debug", false);
 
 	public static interface ExceptionInfo {
