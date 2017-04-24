9099875a2b8fef60e695fc1e2756178a6f3ede24
heyanjing <1366162208@qq.com>
2017-04-24 15:26:58 +0800

    加入test是环境

---
+++ src/test/java/com/he/spring/dao/UserDaoTest.java
@@ -0,0 +1,29 @@
+package com.he.spring.dao;
+
+import com.he.spring.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+/**
+ * Created by heyanjing on 2017/4/24 15:09.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath*:spring/spring-base.xml"})
+public class UserDaoTest {
+    @Autowired
+    UserDao userDao;
+
+    @Test
+    public void findByName() throws Exception {
+        List<User> list = userDao.findByName("何彦静");
+        System.out.println(list);
+
+        
+    }
+
+}
